d90191421b4374e503e47b3c1f847649e524f7c2
Nawar Yossef <nawaryossef2@gmail.com>
2017-11-06 19:21:37 -0500

    add API layer to markup

--- public/js/main.js
+++ public/js/main.js
@@ -1,73 +1,96 @@
-/* global $ */
 "use strict";
-let script = require('./another-script.js');
+// let script = require('./another-script.js');
 
-function getDataFromApi(cityName) {
-	const url = "https://api.foursquare.com/v2/venues/search";
+function FoodDataFromApi(cityName) {
+	const url = "https://api.foursquare.com/v2/venues/explore";
     const id = "CF2LRN214ZC311Z1IHDGZBMA5MHRSH1C2X5UEHU3DOZTRXBM";
     const secret = "NKH0WYKFHDBBONXDQRYCA0GFI3GO45GI1EHPUZGSJN25EJRX";  
 
 	$.ajax({
 		method: 'GET',
-        url: url,
-        dataType: 'jsonp',	
-		client_id: "CF2LRN214ZC311Z1IHDGZBMA5MHRSH1C2X5UEHU3DOZTRXBM",
-		client_secret: "NKH0WYKFHDBBONXDQRYCA0GFI3GO45GI1EHPUZGSJN25EJRX",
-		near: "Chicago, IL",
-		radius:	250,
-		section: "food",	
-		// query: "wine",
-		limit:	10,
-		time:	"any",
-		venuePhotos: 1,
+		url: url,
+		dataType: 'jsonp',	
+		data: {	client_id: id,
+			client_secret: secret,
+			v: Date.now(),
+			near: "new york",
+			radius:	250,
+			section: "wine",	
+			query: "wine",		
+			limit:	8,
+			time:	"any",
+			venuePhotos: true,
+		},
 		success: data => {
-			// showImages(data);
+			const results = data.response.groups[0].items;
+			console.log(results);
+
+			renderResult(results);
 			// mapAllVideo(data);
 			// SearchResulstCount(data);
-			console.log(data)
 		}
 	}) 
 }
 
-function showImages(data) {
-	const images = data.items.map(item => {
-		return (
-			`<div class="col-3">
-			 	<a href="" rel="modal:open" class="anchor" target="_blank" >
-					<img src=${item.snippet.thumbnails.medium.url} class="box"  role="presentation"></img>
-				</a>
-			</div>`
-		)
+function renderResult(data) {
+
+	const venues = data.map((item) => {
+			return (
+				`<div class="venue col-3">
+					<div class="container-for-image">
+						<img src="${item.venue.photos.groups[0].items[0].prefix}200x200${item.venue.photos.groups[0].items[0].suffix}"/>
+					</div>
+					<h3 class="venue-name">${item.venue.name}</h3>
+					<h5 class="venue-type">${item.venue.categories[0].name}</h5>
+					<div class="container-for-rating">
+						<p class="rating" style="background-color: #${item.venue.ratingColor};">${item.venue.rating}</p>
+					</div>
+
+					<div class="container-for-price">
+					<p class="price">Price: <span class="price-description">${getVenuePrice(item)}</span></p>
+				</div>
+
+				<div class="address">
+					<p class="address-desc">
+						${printFormattedAddress(item)}
+					</p>
+				</div>
+				</div>`
+			)	
 	})
-	$(".row").append(images)
+	$(".row").append(venues)
+}
+
+function getVenuePrice(item) {
+	if (Object.keys(item.venue).includes("price")) {
+		return item.venue.price.message;
+	} 
+		$(".price").hide();
+	
+}
+
+function printFormattedAddress(item) {
+	return item.venue.location.formattedAddress.join("").split(",").join(" ")
 }
 
 function getSearchTerm() {
 	$("button").click( e => {
-		const searchTerm = $('.navbar-form :input').val();
+		let searchTerm = $('form :input').val();
 		e.preventDefault();
-		getDataFromApi(searchTerm);
+		FoodDataFromApi(searchTerm);
+		
+		// clear input value for new search
+		clearInputVal()
 	})
 }
 
-function mapAllVideo(data) {
-	$(".anchor").each((idx, ele) => {
-		const id = data.items[idx].id.videoId;
-		const url = 'https://www.youtube.com/watch?v=';
-		$(ele).attr("href", `${url}${id}`);	
-	})
+function clearInputVal() {
+	$('form').trigger("reset");
 }
-
-function SearchResulstCount(data) {
-	const text = `${data.items.length} Results`
-	$("p").text(text);
-}
-
-function resultDefaultState() {
-	$("span > p").text('');
-}
-
 
 
 $(getSearchTerm)
 
+
+// display address
+// if data is an empty array then display other info
