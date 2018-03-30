cfc83a2bcb023631b6e36334c8ec548354d80108
asdasd <nawaryossef2@gmail.com>
2018-03-29 19:09:01 -0600

    travis

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 var app = express();
 
-//set port
 const port = process.env.PORT || 8081;
 
 app.use(express.static(__dirname + "/public"));
-
 
 app.listen(port, function() {
 	console.log( "server is up and running on port 8080")
